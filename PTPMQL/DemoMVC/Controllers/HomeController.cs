d086de9ee67efebfadb6c0351a37967dd9e6098f
tranvietduc1207 <tranvietduc1207@gmail.com>
2025-02-15 18:59:36 +0700

    BTH_1002

--- PTPMQL/DemoMVC/Controllers/HomeController.cs
+++ PTPMQL/DemoMVC/Controllers/HomeController.cs
@@ -1,31 +1,24 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using DemoMVC.Models;
 
-namespace DemoMVC.Controllers;
-
-public class HomeController : Controller
+namespace DemoMVC.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [HttpPost]
+        public IActionResult Contact(string name, string email)
+        {
+            ViewBag.Message = $"Hello {name}, we received your email ({email})!";
+            return View();
+        }
     }
 }
--- PTPMQL/DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ PTPMQL/DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccdbd6d218e7acdd0453e18212a667d36775e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
